72149a12f086effb88ff891bf66905ae2b61e03b
irene <s248039@studenti.polito.it>
2022-05-23 15:19:54 +0200

    Modify APP.JS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import eclipse from './icons/eclipse.svg';
 import './App.css';
 import HeaderIrene from "./components/HeaderIrene";
 
@@ -9,20 +8,6 @@ function App() {
         <div>
             <HeaderIrene/>
         </div>
-        {/*
-      <header className="App-header">
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-        */}
-
     </div>
   );
 }
