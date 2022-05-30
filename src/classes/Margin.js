c1a9175e6ebba395504f9b7694d4afc5dbe3caa1
irene <s248039@studenti.polito.it>
2022-05-30 22:05:16 +0200

    Implementazione classi margin e padding (probabilmente non serviranno)

---
+++ src/classes/Margin.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const margin = [
+    { id: 1, name: "m" },
+    { id: 2, name: "ml" },
+    { id: 3, name: "mr" },
+    { id: 4, name: "mt" },
+    { id: 5, name: "mb" },
+    { id: 6, name: "mx" },
+    { id: 7, name: "my" },
+];
+
+
+
+export default function Margin() {
+    return (
+        <div>
+            <p>
+               Margin
+            </p>
+        </div>
+    );
+}
---
+++ src/classes/Padding.js
@@ -0,0 +1,19 @@
+const padding = [
+  { id: 1, name: "p" },
+  { id: 2, name: "pl" },
+  { id: 3, name: "pr" },
+  { id: 4, name: "pt" },
+  { id: 5, name: "pb" },
+  { id: 6, name: "px" },
+  { id: 7, name: "py" },
+];
+
+export default function Padding() {
+  return (
+      <div>
+        <p>
+          Padding
+        </p>
+      </div>
+  );
+}
